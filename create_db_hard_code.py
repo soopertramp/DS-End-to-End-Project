533f46f56c3c1146127b45fa8133beb984de59fb
soopertramp <pradeepchandrareddy95@gmail.com>
2023-02-09 17:58:58 +0530

    hard_code

--- create_db_hard_code.py
+++ create_db_hard_code.py
@@ -2,7 +2,7 @@
 
 import mysql.connector as mysql
 
-# Establish a connection to the database
+#establish a connection to the database
 
 mydb = mysql.connect(
   host="localhost",
@@ -18,18 +18,18 @@ and retrieve the results. """
 
 mycursor = mydb.cursor()
 
-#Executing "SHOW DATABASES" shows the databases on your computer
+#executing "SHOW DATABASES" shows the databases on your computer
 
 mycursor.execute("SHOW DATABASES")
 
 for x in mycursor:
   print(x)
   
-#Create a database
+#create a database
 
 mycursor.execute("CREATE DATABASE IF NOT EXISTS supermarket")
 
-#Checking if that works
+#checking if that works
 
 mycursor.execute("SHOW DATABASES")
 
@@ -44,11 +44,11 @@ mycursor.execute("USE supermarket")
 
 mycursor.execute("DROP TABLE supermarket_sales")
   
-# Create a table
+#create a table
 
 mycursor.execute("CREATE TABLE supermarket_sales (invoice_id VARCHAR(255), branch CHAR, city VARCHAR(255), customer_type VARCHAR(255), gender VARCHAR(255), product_line VARCHAR(255), unit_price FLOAT, quantity INT, tax_5_percent FLOAT, total FLOAT, date DATE, time TIME, payment VARCHAR(255), cogs FLOAT, gross_margin_percentage FLOAT, gross_income FLOAT, rating FLOAT)")    
   
-# Insert data into a table
+#insert data into a table
 
 import csv
 
@@ -63,8 +63,10 @@ with open('supermarket_sales.csv', 'r') as file:
         # Execute the SQL statement
         mycursor.execute(sql, row)
         
+#commit the transaction
 mydb.commit()
 
+#retrieve data from a table
 mycursor.execute("select * from supermarket_sales")
 
 myresult = mycursor.fetchall()
@@ -73,3 +75,60 @@ for x in myresult:
   print(x)
   
 mydb.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#cleaning the date column
+
+query = "SELECT date FROM supermarket_sales"
+mycursor.execute(query)
+dates = mycursor.fetchall()
+
+# clean the date values in the column
+
+cleaned_dates = []
+for date in dates:
+    cleaned_date = date[0].strftime("%Y-%m-%d")
+    cleaned_dates.append(cleaned_date)
+    
+# update the cleaned date values in the MySQL table
+
+for i, cleaned_date in enumerate(cleaned_dates):
+    update_query = "UPDATE supermarket_sales SET date = %s WHERE id = %s"
+    mycursor.execute(update_query, (cleaned_date, i+1))
+    
+mydb.commit()
