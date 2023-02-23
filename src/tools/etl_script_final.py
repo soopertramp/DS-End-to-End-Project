3a4bbd9c687837f81a9c43df6a5a38d6d5112150
soopertramp <pradeepchandrareddy95@gmail.com>
2023-02-23 13:55:40 +0530

    updates

--- src/tools/etl_script_final.py
+++ src/tools/etl_script_final.py
@@ -4,13 +4,9 @@ import matplotlib.pyplot as plt
 
 #reading the data
 df = pd.read_csv('data/supermarket_sales.csv')
-df.head()
 
 # convert the 'date' column to datetime format
 df['date'] = pd.to_datetime(df['date'])
-
-# convert the 'time' column to datetime format
-#df['time'] = pd.to_datetime(df['time'])
 
 #sorting the data by date in ascending order
 df.sort_values(by='date', ascending=True, ignore_index=True, inplace = True)
@@ -23,7 +19,6 @@ table2 = df[['invoice_id', 'quantity', 'tax_5_percent', 'total', 'date', 'time',
 table3 = df[['invoice_id', 'cogs', 'gross_margin_percentage', 'gross_income', 'rating',]]
 
 # Save the tables as CSV files
-df.to_csv('supermarket_sales_cleaned.csv', index=False)
 table1.to_csv('table1.csv', index=False)
 table2.to_csv('table2.csv', index=False)
 table3.to_csv('table3.csv', index=False)
