b7c67873c1c664204d160d957c6c27b81a38274e
mitulds <mitulppatel8@gmail.com>
2023-03-11 21:42:18 -0700

    final clean up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 import yaml
-from src.tools.utils import upload_to_google_sheet, upload_to_s3, process_task
+from src.tools.utils import upload_to_s3, process_task
 
 args = argparse.ArgumentParser(
     description="Provies some inforamtion on the job to process"
@@ -19,6 +19,6 @@ config_export = config[args_task]["export"]
 
 if config_export[0]["export"]["host"] == 's3':
     upload_to_s3(process_task(args_task), config_export[0]["export"]["filename"])
-elif config_export[0]["export"]["host"] == 'gsheet':
-    upload_to_google_sheet(config_export[0]["export"]["spreadsheet_id"], process_task(args.task), 
-                     config_export[0]["export"]["clear_sheet"])
+#elif config_export[0]["export"]["host"] == 'gsheet':
+ #   upload_to_google_sheet(config_export[0]["export"]["spreadsheet_id"], process_task(args.task), 
+  #                   config_export[0]["export"]["clear_sheet"])
